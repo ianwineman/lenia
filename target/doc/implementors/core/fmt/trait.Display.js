7fe1fb196f13c51a325957b9af572c16c38b9b4d
ianwineman <ian@ianwineman.com>
2023-06-16 14:58:25 -0400

    remove docs

--- target/doc/implementors/core/fmt/trait.Display.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"lenia":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"lenia/gol/struct.World.html\" title=\"struct lenia::gol::World\">World</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
